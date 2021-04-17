46391f5fe91b0d222e0465eff32fcc465de67835
Tigran AL <tigran@appliedlabs.io>
2021-04-18 03:11:10 +0500

    added labels for filters

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -32,37 +32,49 @@ export const Filters = () => {
         <div className="filter-item mr-5">
           <h5>Field:</h5>
           <div className="form-check">
-            <input className="form-check-input" value="id" type="radio"
-              checked={tasksFilters.field === 'id'} onChange={radioChange} />
-            <span>ID</span>
+            <label>
+              <input className="form-check-input" value="id" type="radio"
+                checked={tasksFilters.field === 'id'} onChange={radioChange} />
+              <span>ID</span>
+            </label>
           </div>
           <div className="form-check">
-            <input className="form-check-input" value="username" type="radio"
-              checked={tasksFilters.field === 'username'} onChange={radioChange} />
-            <span>Username</span>
+            <label>
+              <input className="form-check-input" value="username" type="radio"
+                checked={tasksFilters.field === 'username'} onChange={radioChange} />
+              <span>Username</span>
+            </label>
           </div>
           <div className="form-check">
-            <input className="form-check-input" value="email" type="radio"
-              checked={tasksFilters.field === 'email'} onChange={radioChange} />
-            <span>Email</span>
+            <label>
+              <input className="form-check-input" value="email" type="radio"
+                checked={tasksFilters.field === 'email'} onChange={radioChange} />
+              <span>Email</span>
+            </label>
           </div>
           <div className="form-check">
-            <input className="form-check-input" value="status" type="radio"
-              checked={tasksFilters.field === 'status'} onChange={radioChange} />
-            <span>Status</span>
+            <label>
+              <input className="form-check-input" value="status" type="radio"
+                checked={tasksFilters.field === 'status'} onChange={radioChange} />
+              <span>Status</span>
+            </label>
           </div>
         </div>
         <div className="filter-item mr-5">
           <h5>Direction:</h5>
           <div className="form-check">
-            <input className="form-check-input" value="asc" type="radio"
-              checked={tasksFilters.direction === 'asc'} onChange={radioChange} />
-            <span>ASC</span>
+            <label>
+              <input className="form-check-input" value="asc" type="radio"
+                checked={tasksFilters.direction === 'asc'} onChange={radioChange} />
+              <span>ASC</span>
+            </label>
           </div>
           <div className="form-check">
-            <input className="form-check-input" value="desc" type="radio"
-              checked={tasksFilters.direction === 'desc'} onChange={radioChange} />
-            <span>DESC</span>
+            <label>
+              <input className="form-check-input" value="desc" type="radio"
+                checked={tasksFilters.direction === 'desc'} onChange={radioChange} />
+              <span>DESC</span>
+            </label>
           </div>
         </div>
       </div>
